8fa8a8950837e298f492ff081f736a4c6000da9e
AkshayaSiddiTW <141261227+AkshayaSiddiTW@users.noreply.github.com>
2023-08-15 10:13:57 -0400

    Update employees.py

--- FMP/employees.py
+++ FMP/employees.py
@@ -11,7 +11,7 @@ tickers_list = df['symbol'].tolist()
 
 def get_emp_count(ticker):
     try:
-        url = f"https://financialmodelingprep.com/api/v4/historical/employee_count?symbol={ticker}&apikey=c1be0adf4239811e53c74255e93f467e"
+        url = f"https://financialmodelingprep.com/api/v4/historical/employee_count?symbol={ticker}&apikey={apikey}"
 
         df = pd.DataFrame(requests.get(url).json())
         df = df[pd.to_datetime(df['periodOfReport']).dt.year == 2015]
